9c03fb3c7e17901377a1e7faa8338872c5b8e543
javi <javoraptor@gmail.com>
2016-12-16 16:06:53 -0600

    adding ng chart for userInvestments

--- client/app/user-investment-component/user-investment.component.ts
+++ client/app/user-investment-component/user-investment.component.ts
@@ -16,8 +16,10 @@ import { Purchase } from '../purchase/purchase';
 })
 
 export class UserInvestmentComponent{
+
   constructor(private router: Router,
    private usersService: UsersService) {
+    this.isLoading = false;
     }
   ngOnInit(): void {
   	this.getPurchases();
@@ -30,13 +32,14 @@ export class UserInvestmentComponent{
   public userEmails: type[];
   public emailList: Array<string> = [];
   public amtArry: Array<number> = [];
-    // public doughnutChartLabels:string[] = this.emailList;
+  private isLoading;
+  // public doughnutChartLabels:string[] = this.emailList;
   // public doughnutChartData:number[] = this.amtArry;
-  public doughnutChartLabels:Array<string> =['click'];
+  public doughnutChartLabels:Array<string> =[];
   //["Download Sales", 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [0];
+  public doughnutChartData:number[] = [];
   
-  public doughnutChartType:string = 'pie';
+  public doughnutChartType:string = 'doughnut';
   // events
   public chartClicked(e:any):void {
     // console.log(e);
@@ -45,7 +48,7 @@ export class UserInvestmentComponent{
     // console.log(e);
   }
 
- addUserEmail(user): void {
+ addUserEmail() {
  	// console.log("inside add user email");
  	// console.log(user);
  	var arrayLength = this.purchases.length;
@@ -55,18 +58,19 @@ export class UserInvestmentComponent{
     	this.doughnutChartLabels.push(this.purchases[i].user);
     	this.doughnutChartData.push(this.purchases[i].amount);
 		}
-		console.log("emailList");
-		console.log(this.doughnutChartLabels);
-		console.log("amtArry");
-		console.log(this.doughnutChartData);
+		// console.log("emailList");
+		// console.log(this.doughnutChartLabels);
+		// console.log("amtArry");
+		// console.log(this.doughnutChartData);
 }
 
   getPurchases(): void {
     this.usersService.getPurchases().then(
       purchase => {
         this.purchases = purchase;
+        this.addUserEmail();
+        this.isLoading = true;
       }
-      ).then(user => this.addUserEmail(user)
       ).catch(function(e) {
       	console.log('Inside getPurchase Exception'); 
   		console.log(e); 
@@ -86,15 +90,7 @@ export class UserInvestmentComponent{
   		console.log(e); 
 	});
   }
-
-
-
-
-// user => this.addUserEmail(user)
-
 }
-
-
 export interface type{
     email:string;
 }
