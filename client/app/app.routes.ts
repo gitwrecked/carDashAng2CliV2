b29b349b74890084960bf82dda6f0c16e55f4dd3
Arun Varghese <avarghese@spireon.com>
2016-12-20 22:31:17 -0600

    TG-21: sessionServices, appRoutes

---
+++ client/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule }              from '@angular/router';
+import { AuthGuard } 				 from './common/auth.guard';
+import { PurchaseHistoryComponent }  from './purchase-history-component/purchase.history.component';
+import { UserInvestmentComponent }   from './user-investment-component/user-investment.component';
+import { UserFormComponent }         from './user-form/user-form.component';
+import { LoginComponent }            from './login/login.component';
+
+export const AppRoutes: any = [
+  { path: 'userForm',       		component: UserFormComponent, canActivate: [AuthGuard] },
+  { path: 'purchaseHistory',    	component: PurchaseHistoryComponent },
+  { path: 'usersInvestments',   	component: UserInvestmentComponent },
+  { path: 'login',  				component: LoginComponent },
+  { path: 'home',   				component: UserInvestmentComponent },
+  { path: '**',     				component: UserInvestmentComponent },
+];
